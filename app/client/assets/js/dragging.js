7188afc700ddfcc5245b7582153a645f64918f40
«Softie123456789» <lennert.vermoesen@outlook.com>
2023-05-23 12:45:32 +0200

    new draging

--- app/client/assets/js/dragging.js
+++ app/client/assets/js/dragging.js
@@ -1,87 +1,101 @@
 const app = new PIXI.Application({
     backgroundColor: 0xffc0cb,
-    with: 800,
-    height: 800
-
-});
-const blokContainer = document.getElementById('blok');
-blokContainer.appendChild(app.view);
-
-const pianoImage = document.getElementById('piano');
-
-const texture = PIXI.Texture.from(pianoImage);
-
-texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-
-createPiano(
-    200, // x position in pixels
-    500, // y position in pixels
-    150, // width in pixels
-    160 // height in pixels
-);
-
-
-function createPiano(x, y, widthPixels, heightPixels) {
-    const piano = new PIXI.Sprite(texture);
-    piano.interactive = true;
-    piano.cursor = 'pointer';
-    piano.anchor.set(0.5);
-
-    piano.width = widthPixels;
-    piano.height = heightPixels;
-
-    piano.on('pointerdown', onDragStart);
-
-    piano.x = x;
-    piano.y = y;
-
-    app.stage.addChild(piano);
-}
-
-let dragTarget = null;
-
-app.stage.interactive = true;
-app.stage.hitArea = app.screen;
-app.stage.on('pointerup', onDragEnd);
-app.stage.on('pointerupoutside', onDragEnd);
-
-blokContainer.addEventListener('mouseenter', () => {
-  app.stage.on('pointermove', onDragMove);
-});
-
-blokContainer.addEventListener('mouseleave', () => {
-  app.stage.off('pointermove', onDragMove);
-  if (isDragging) {
-    onDragEnd();
+  });
+  const blokContainer = document.getElementById("canvas");
+  blokContainer.appendChild(app.view);
+  
+  let texture; // Variabele voor de PIXI-textuur
+  
+  const clickHandler = (event) => {
+    const itemImage = document.getElementById("stoel"); // Verander "item" naar de ID van je HTML-afbeelding
+  
+    texture = PIXI.Texture.from(itemImage);
+    texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+  
+    const itemWidth = 150; // Breedte van het item in pixels
+    const itemHeight = 160; // Hoogte van het item in pixels
+  
+    const canvasBounds = app.view.getBoundingClientRect();
+    const canvasOffsetX = canvasBounds.left;
+    const canvasOffsetY = canvasBounds.top;
+  
+    const clickX = event.clientX - canvasOffsetX;
+    const clickY = event.clientY - canvasOffsetY;
+  
+    spawnItem(clickX, clickY, itemWidth, itemHeight);
+  };
+  
+  function spawnItem(x, y, widthPixels, heightPixels) {
+    const item = new PIXI.Sprite(texture);
+    item.interactive = true;
+    item.cursor = "pointer";
+    item.anchor.set(0.5);
+  
+    item.width = widthPixels;
+    item.height = heightPixels;
+  
+    item.on("pointerdown", onDragStart, item);
+  
+    item.x = x;
+    item.y = y;
+  
+    app.stage.addChild(item);
   }
-});
-
-function onDragMove(event) {
-        dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-    
-    const newPositionX = dragTarget.x + event.data.originalEvent.movementX;
-    const newPositionY = dragTarget.y + event.data.originalEvent.movementY;
-
-    const minX = dragTarget.width / 2;
-    const maxX = app.view.width - dragTarget.width / 2;
-    const minY = dragTarget.height / 2;
-    const maxY = app.view.height - dragTarget.height / 2;
-
-    dragTarget.x = Math.max(minX, Math.min(maxX, newPositionX));
-    dragTarget.y = Math.max(minY, Math.min(maxY, newPositionY));
-
-}
-
-function onDragStart(event) {
+  
+  blokContainer.addEventListener("mouseleave", () => {
+    app.stage.off("pointermove", onDragMove);
+    onDragEnd();
+  });
+  
+  let dragTarget = null;
+  let isDragging = false;
+  
+  app.stage.hitArea = app.screen;
+  app.stage.on("pointerup", onDragEnd);
+  app.stage.on("pointerupoutside", onDragEnd);
+  
+  blokContainer.addEventListener("mouseenter", () => {
+    app.stage.on("pointermove", onDragMove);
+  });
+  
+  blokContainer.addEventListener("mouseleave", () => {
+    app.stage.off("pointermove", onDragMove);
+    if (isDragging) {
+      onDragEnd();
+    }
+  });
+  
+  function onDragMove(event) {
+    if (dragTarget) {
+      dragTarget.parent.toLocal(event.data.global, null, dragTarget.position);
+  
+      const newPositionX = dragTarget.x + event.data.originalEvent.movementX;
+      const newPositionY = dragTarget.y + event.data.originalEvent.movementY;
+  
+      const minX = dragTarget.width / 2;
+      const maxX = app.view.width - dragTarget.width / 2;
+      const minY = dragTarget.height / 2;
+      const maxY = app.view.height - dragTarget.height / 2;
+  
+      dragTarget.x = Math.max(minX, Math.min(maxX, newPositionX));
+      dragTarget.y = Math.max(minY, Math.min(maxY, newPositionY));
+    }
+  }
+  
+  function onDragStart(event) {
     this.alpha = 0.5;
     dragTarget = this;
-    app.stage.on('pointermove', onDragMove);
-}
-
-function onDragEnd() {
+    app.stage.on("pointermove", onDragMove);
+    isDragging = true; // Set isDragging to true when dragging starts
+  }
+  
+  function onDragEnd() {
     if (dragTarget) {
-        app.stage.off('pointermove', onDragMove);
-        dragTarget.alpha = 1;
-        dragTarget = null;
+      app.stage.off("pointermove", onDragMove);
+      dragTarget.alpha = 1;
+      dragTarget = null;
+      isDragging = false; // Set isDragging to false when dragging ends
     }
-}
+  }
+  
+  blokContainer.addEventListener("click", clickHandler);
