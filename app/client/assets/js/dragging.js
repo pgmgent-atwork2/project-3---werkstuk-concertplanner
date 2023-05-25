ce2108a6e6734d959f89b8a6b66855da9d2986d3
«Softie123456789» <lennert.vermoesen@outlook.com>
2023-05-25 15:39:09 +0200

    pixi added image spawn

--- app/client/assets/js/dragging.js
+++ app/client/assets/js/dragging.js
@@ -1,78 +1,89 @@
-const $app = new PIXI.Application({
-    backgroundColor: 0xffc0cb,
-  });
-
-  if ($app.classList) {
-    $app.classList.add('pixi-frame');
-  } else {
-    // Handle the case when $app.classList is not available
-    console.error("$app.classList is undefined");
-  }
+document.addEventListener("DOMContentLoaded", () => {
+    const app = new PIXI.Application({
+      backgroundColor: 0xffc0cb,
+    });
   
-  const blokContainer = document.getElementById("canvas");
- 
-  blokContainer.addEventListener("mouseleave", () => {
-    $app.stage.off("pointermove", onDragMove);
-    onDragEnd();
-  });
+    const blokContainer = document.getElementById("canvas");
   
-  const dragTarget = null;
-  const isDragging = false;
+    // Create an array to store the selected list items
+    const selectedItems = [];
   
-  $app.stage.hitArea = $app.screen;
-  $app.stage.on("pointerup", onDragEnd);
-  $app.stage.on("pointerUpOutside", onDragEnd);
+    // Select list items
+    const listItems = document.querySelectorAll("#podiumelementen li img");
   
-  blokContainer.addEventListener("mouseenter", () => {
-    $app.stage.on("pointermove", onDragMove);
-  });
+    // Loop through all list items
+    listItems.forEach((item) => {
+      // Add event listener to each list item
+      item.addEventListener("click", () => {
+        // Get unique info from the element (modify this part according to your needs)
+        const uniqueInfo = item.alt;
   
-  blokContainer.addEventListener("mouseleave", () => {
-    $app.stage.off("pointermove", onDragMove);
-    if (isDragging) {
-      onDragEnd();
+        // Create a container for each draggable element
+        const container = new PIXI.Container();
+        container.position.set(100, 100); // Set the initial position according to your requirements
+        container.interactive = true;
+        container.buttonMode = true;
+  
+        // Create a sprite from the item image
+        const sprite = PIXI.Sprite.from(item.src);
+        sprite.anchor.set(0.5);
+        sprite.width = 50; // Set the width to 100 pixels
+        sprite.height = 50; // Set the height to 100 pixels
+        container.addChild(sprite);
+  
+        // Event listeners for drag functionality
+        container
+          .on("pointerdown", onDragStart)
+          .on("pointerup", onDragEnd)
+          .on("pointerupoutside", onDragEnd)
+          .on("pointermove", onDragMove);
+  
+        app.stage.addChild(container);
+  
+        // Add the container to the selectedItems array
+        selectedItems.push(container);
+      });
+    });
+  
+    // Append the Pixi canvas to the blokContainer
+    blokContainer.appendChild(app.view);
+  
+    // Add class to Pixi canvas container
+    blokContainer.classList.add("pixi-frame");
+  
+    let dragTarget = null;
+  
+    app.stage.interactive = true;
+    app.stage.hitArea = app.screen;
+    app.stage.on("pointerup", onDragEnd);
+    app.stage.on("pointerupoutside", onDragEnd);
+  
+    function onDragMove(event) {
+      if (dragTarget) {
+        dragTarget.parent.toLocal(event.data.global, null, dragTarget.position);
+      }
+    }
+  
+    function onDragStart(event) {
+      this.alpha = 0.5;
+      dragTarget = this;
+      app.stage.on("pointermove", onDragMove);
+      event.stopPropagation();
+    }
+  
+    function onDragEnd() {
+      if (dragTarget) {
+        app.stage.off("pointermove", onDragMove);
+        dragTarget.alpha = 1;
+        dragTarget = null;
+      }
     }
   });
   
-  function onDragMove(event) {
-    if (dragTarget) {
-      dragTarget.parent.toLocal(event.data.global, null, dragTarget.position);
   
-      const newPositionX = dragTarget.x + event.data.originalEvent.movementX;
-      const newPositionY = dragTarget.y + event.data.originalEvent.movementY;
-  
-      const minX = dragTarget.width / 2;
-      const maxX = $app.view.width - dragTarget.width / 2;
-      const minY = dragTarget.height / 2;
-      const maxY = $app.view.height - dragTarget.height / 2;
-  
-      dragTarget.x = Math.max(minX, Math.min(maxX, newPositionX));
-      dragTarget.y = Math.max(minY, Math.min(maxY, newPositionY));
-    }
-  }
-  
-  function onDragStart(event) {
-    this.alpha = 0.5;
-    dragTarget = this;
-    $app.stage.on("pointermove", onDragMove);
-    isDragging = true; // Set isDragging to true when dragging starts
-  }
-  
-  function onDragEnd() {
-    if (dragTarget) {
-      $app.stage.off("pointermove", onDragMove);
-      dragTarget.alpha = 1;
-      dragTarget = null;
-      isDragging = false; // Set isDragging to false when dragging ends
-    }
-  }
-  
-  blokContainer.addEventListener("click", clickHandler);
 
-  // Select list items
-  // loop through all list items
-  // Add eventlistener to each list item
-  // When clicked, get unique info from the element. 
-  // add element to pixi canvas (app.stage.addChild)
-
-  
+// Select list items
+// loop through all list items
+// Add eventlistener to each list item
+// When clicked, get unique info from the element.
+// add element to pixi canvas (app.stage.addChild)
