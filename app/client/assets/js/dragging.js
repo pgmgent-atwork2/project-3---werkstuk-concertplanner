a83049d0f9f5ce32ce659dfebb461667663ac67b
«Softie123456789» <lennert.vermoesen@outlook.com>
2023-06-01 16:20:58 +0200

    rotation toegevoegd

--- app/client/assets/js/dragging.js
+++ app/client/assets/js/dragging.js
@@ -44,11 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // Select list items
   const listItems = document.querySelectorAll("#podiumelementen li img");
 
-  // Function to check for collision between two items
   function makeBounds(object1, object2) {
-    const bounds1 = object1.getBounds();
-    const bounds2 = object2.getBounds();
-
+    const bounds1 = object1.getChildAt(1).getBounds(); // Exclude the border from bounds
+    const bounds2 = object2.getChildAt(1).getBounds(); // Exclude the border from bounds
+  
     if (
       bounds1.x < bounds2.x + bounds2.width &&
       bounds1.x + bounds1.width > bounds2.x &&
@@ -59,6 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return false;
   }
+  
 
   // Function to get the bounds of an item
   function getBounds(item) {
@@ -92,14 +92,25 @@ document.addEventListener("DOMContentLoaded", () => {
       sprite.anchor.set(0.5);
       sprite.width = 40; // Set the width to 100 pixels
       sprite.height = 40; // Set the height to 100 pixels
+      // Create a border graphics object
+      const border = new PIXI.Graphics();
+      border.lineStyle(2, 0xff0000); // Set the border color and thickness
+      border.drawRect(
+        -sprite.width / 2,
+        -sprite.height / 2,
+        sprite.width,
+        sprite.height
+      );
+
+      container.addChild(border); // Add the border to the container
       container.addChild(sprite);
 
       // Event listeners for drag functionality
       container
         .on("pointerdown", onDragStart)
+        .on("pointermove", onDragMove)
         .on("pointerup", onDragEnd)
-        .on("pointerupoutside", onDragEnd)
-        .on("pointermove", onDragMove);
+        .on("pointerupoutside", onDragEnd);
 
       app.stage.addChild(container);
 
@@ -128,92 +139,174 @@ document.addEventListener("DOMContentLoaded", () => {
         null,
         dragTarget.position
       );
-
+  
       let collisionDetected = false;
-      selectedItems.forEach((item) => {
+      let neighborDetected = false;
+      let overlapDetected = false; // New variable to check for overlap
+  
+      for (let i = 0; i < selectedItems.length; i++) {
+        const item = selectedItems[i];
         if (item !== dragTarget && makeBounds(dragTarget, item)) {
           collisionDetected = true;
-          const dx = newPosition.x - item.position.x;
-          const dy = newPosition.y - item.position.y;
-          const absDX = Math.abs(dx);
-          const absDY = Math.abs(dy);
-
-          if (absDX > absDY) {
-            dragTarget.position.x =
-              dx > 0
-                ? item.position.x + item.width + 1
-                : item.position.x - dragTarget.width - 1;
-          } else {
-            dragTarget.position.y =
-              dy > 0
-                ? item.position.y + item.height + 1
-                : item.position.y - dragTarget.height - 1;
-          }
+          break; // Exit the loop once a collision is detected
         }
-      });
-
-      if (!collisionDetected) {
-        // Calculate the closest grid position
+        if (
+          item !== dragTarget &&
+          item.position.x !== newPosition.x &&
+          item.position.y !== newPosition.y &&
+          Math.abs(newPosition.x - item.position.x) < gridSize &&
+          Math.abs(newPosition.y - item.position.y) < gridSize
+        ) {
+          neighborDetected = true;
+          break; // Exit the loop once a neighboring item is detected
+        }
+        if (
+          item !== dragTarget &&
+          makeBounds(dragTarget, item) &&
+          (Math.abs(newPosition.x - item.position.x) < gridSize ||
+            Math.abs(newPosition.y - item.position.y) < gridSize)
+        ) {
+          overlapDetected = true;
+          break; // Exit the loop once overlap is detected
+        }
+      }
+  
+      if (!collisionDetected && !neighborDetected && !overlapDetected) {
         const snapX = Math.round(newPosition.x / gridSize) * gridSize;
         const snapY = Math.round(newPosition.y / gridSize) * gridSize;
-
-        // Update the position to the snapped position
+  
         dragTarget.position.set(snapX, snapY);
+        dragTarget.lastValidPosition.copyFrom(dragTarget.position);
+      } else {
+        dragTarget.position.copyFrom(dragTarget.lastValidPosition);
       }
     }
   }
+  
+  
+  
+   
 
   function onDragStart(event) {
     this.alpha = 0.5;
     dragTarget = this;
-    dragTarget.lastValidPosition = dragTarget.position.clone(); // Store the last valid position
+    dragTarget.lastValidPosition = dragTarget.position.clone(); // Store the initial position
     app.stage.on("pointermove", onDragMove);
     event.stopPropagation();
 
     const bounds = getBounds(this);
     console.log("Bounds:", bounds);
+
+    // Add event listeners for rotation using arrow keys
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
   }
 
-  function onDragEnd() {
-    if (dragTarget) {
-      app.stage.off("pointermove", onDragMove);
-      dragTarget.alpha = 1;
-
-      let collisionDetected = false;
-      let closestItem = null;
-      let closestDistance = Number.MAX_VALUE;
-
-      selectedItems.forEach((item) => {
-        if (item !== dragTarget && makeBounds(dragTarget, item)) {
-          collisionDetected = true;
-          const distance = Math.sqrt(
-            Math.pow(dragTarget.position.x - item.position.x, 2) +
-              Math.pow(dragTarget.position.y - item.position.y, 2)
-          );
-          if (distance < closestDistance) {
-            closestDistance = distance;
-            closestItem = item;
-          }
-        }
-      });
-
-      if (collisionDetected) {
-        // Calculate the closest grid position relative to the collided item
-        const snapX = Math.round(closestItem.position.x / gridSize) * gridSize;
-        const snapY = Math.round(closestItem.position.y / gridSize) * gridSize;
-
-        // Snap to the closest grid position
-        dragTarget.position.set(snapX, snapY);
-      } else {
-        // Calculate the closest grid position
-        const snapX = Math.round(dragTarget.position.x / gridSize) * gridSize;
-        const snapY = Math.round(dragTarget.position.y / gridSize) * gridSize;
-
-        // Snap to the closest grid position
-        dragTarget.position.set(snapX, snapY);
-      }
-
-      dragTarget = null;
+  function onKeyDown(event) {
+    if (event.key === "ArrowLeft") {
+      // Rotate left
+      dragTarget.rotation -= 0.1;
+    } else if (event.key === "ArrowRight") {
+      // Rotate right
+      dragTarget.rotation += 0.1;
     }
   }
+
+  function onKeyUp(event) {
+    if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+      // Stop rotating
+      dragTarget.rotation =
+        Math.round(dragTarget.rotation / (Math.PI / 4)) * (Math.PI / 4);
+    }
+  }
+
+function onDragEnd() {
+  if (dragTarget) {
+    app.stage.off("pointermove", onDragMove);
+    dragTarget.alpha = 1;
+
+    let collisionDetected = false;
+    let closestItem = null;
+    let closestDistance = Number.MAX_VALUE;
+
+    for (let i = 0; i < selectedItems.length; i++) {
+      const item = selectedItems[i];
+      if (item !== dragTarget && makeBounds(dragTarget, item)) {
+        collisionDetected = true;
+        const distance = Math.sqrt(
+          Math.pow(dragTarget.position.x - item.position.x, 2) +
+          Math.pow(dragTarget.position.y - item.position.y, 2)
+        );
+        if (distance < closestDistance) {
+          closestDistance = distance;
+          closestItem = item;
+        }
+      }
+    }
+
+    if (collisionDetected) {
+      console.log("Collision detected!");
+      console.log("Closest item:", closestItem);
+      console.log("Closest distance:", closestDistance);
+
+      // Calculate the separation vector between the dragged item and the closest item
+      const separationVector = new PIXI.Point(
+        dragTarget.position.x - closestItem.position.x,
+        dragTarget.position.y - closestItem.position.y
+      );
+
+      // Normalize the separation vector
+      const separationLength = Math.sqrt(
+        separationVector.x * separationVector.x + separationVector.y * separationVector.y
+      );
+      separationVector.x /= separationLength;
+      separationVector.y /= separationLength;
+
+      // Calculate the offset to move the rotated item away from the collision
+      const offset = gridSize * 2; // Adjust the offset as needed
+
+      // Calculate the new position by applying the separation vector and offset
+      const newPosition = new PIXI.Point(
+        dragTarget.position.x + separationVector.x * offset,
+        dragTarget.position.y + separationVector.y * offset
+      );
+
+      // Snap the new position to the closest grid position
+      const snapX = Math.round(newPosition.x / gridSize) * gridSize;
+      const snapY = Math.round(newPosition.y / gridSize) * gridSize;
+
+      // Adjust the snap position based on the rotated dimensions of the items
+      const dragBounds = getBounds(dragTarget);
+      const closestBounds = getBounds(closestItem);
+      const adjustedSnapX = snapX + (closestBounds.width - dragBounds.width) / 2;
+      const adjustedSnapY = snapY + (closestBounds.height - dragBounds.height) / 2;
+
+      // Set the new position for the rotated item
+      dragTarget.position.set(adjustedSnapX, adjustedSnapY);
+
+      // Update the last valid position
+      dragTarget.lastValidPosition.copyFrom(dragTarget.position);
+    } else {
+      console.log("No collision detected!");
+
+      // Calculate the closest grid position
+      const snapX = Math.round(dragTarget.position.x / gridSize) * gridSize;
+      const snapY = Math.round(dragTarget.position.y / gridSize) * gridSize;
+
+      // Snap to the closest grid position
+      dragTarget.position.set(snapX, snapY);
+
+      // Update the last valid position
+      dragTarget.lastValidPosition.copyFrom(dragTarget.position);
+    }
+
+    // Remove rotation event listeners
+    window.removeEventListener("keydown", onKeyDown);
+    window.removeEventListener("keyup", onKeyUp);
+
+    dragTarget = null;
+  }
+}
+
+
 });
