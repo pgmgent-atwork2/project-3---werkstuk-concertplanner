c5e5d0de9d839b7be53c81dbd5ba61a569e1a84b
«Softie123456789» <lennert.vermoesen@outlook.com>
2023-06-08 15:45:48 +0200

    server

--- app/server/controllers/plan.js
+++ app/server/controllers/plan.js
@@ -2,12 +2,15 @@ import DataSource from "../lib/DataSource.js";
 
 const userRepo = DataSource.getRepository("User");
 const inventoryRepo = DataSource.getRepository("Inventory");
+const collectionRepo = DataSource.getRepository("Collection");
 
 export const getPlanPage = async (req, res, next) => {
   try {
        
     const getInventory = await inventoryRepo.find(); 
+    const getCollections = await collectionRepo.find();
     console.log(getInventory);
+    console.log(getCollections);
 
     res.render("plan", {
       user: req.user,
