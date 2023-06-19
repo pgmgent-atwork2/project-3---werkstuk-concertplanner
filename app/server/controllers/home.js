1bbc0f60fbb2e9533ac4eeb40584f653e4b5215c
Kyandroo <kyandevo@student.arteveldehs.be>
2023-06-19 16:05:42 +0200

    Fixed bug within slideshow + fixed header trough roles

--- app/server/controllers/home.js
+++ app/server/controllers/home.js
@@ -1,17 +1,35 @@
 import DataSource from "../lib/DataSource.js";
 
+const userRepo = await DataSource.getRepository("User");
+const userMetaRepo = await DataSource.getRepository("UserMeta");
+const roleRepo = await DataSource.getRepository("Role");
+const collectionRepo = await DataSource.getRepository("Collection");
+
 export const home = async (req, res) => {
-  const userRepo = await DataSource.getRepository("User").findOne({
+  const loggedInUser = await userRepo.findOne({
     where: { id: req.user.id },
     relations: ["user_meta", "role"],
   });
-  const userMetaRepo = await DataSource.getRepository("UserMeta");
-  const roleRepo = await DataSource.getRepository("Role");
 
   try {
-    res.render("home", {
-      user: userRepo,
+    const collections = await collectionRepo.find({
+      relations: ["inventory"],
     });
+
+    res.render("home", {
+      user: loggedInUser,
+    });
+
+    // if (loggedInUser.role.label === "admin") {
+    //   res.render("admin/requests-in", {
+    //     user: loggedInUser,
+    //   });
+    // } else {
+    //   res.render("user/inventory", {
+    //     user: loggedInUser,
+    //     collection: collections,
+    //   });
+    // }
   } catch (error) {
     console.log(error);
   }
