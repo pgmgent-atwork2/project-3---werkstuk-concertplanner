9f4b0b246713f5dd2a0acb25f28728645d62db27
Lennert Vermoesen <113594885+Softie123456789@users.noreply.github.com>
2023-06-19 20:17:29 +0200

    styling page plan

--- app/client/js/app.js
+++ app/client/js/app.js
@@ -1,7 +1,10 @@
 import { renderSlideShow } from "./animations/slideshow.js";
 import { smoothScroll } from "./animations/smooth-scroll.js";
 import { dragging } from "./dragging.js";
+import { SVG } from "../../pixi-svg";
 
 renderSlideShow();
 smoothScroll();
 dragging();
+SVG
+
