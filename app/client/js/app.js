f47e72d4d1c99e375203cf8258ebc5e3f8f0109d
Lennert Vermoesen <113594885+Softie123456789@users.noreply.github.com>
2023-06-20 15:02:10 +0200

    search balk

--- app/client/js/app.js
+++ app/client/js/app.js
@@ -4,6 +4,7 @@ import { dragging } from "./dragging.js";
 import { loadDetails } from "./extra/load-details.js";
 import { getActiveNavItem } from "./extra/active.js";
 import { SortByDate } from "./extra/sort-date.js";
+import { zoeken } from "./extra/search.js";
 
 getActiveNavItem();
 renderSlideShow();
@@ -11,3 +12,4 @@ smoothScroll();
 dragging();
 loadDetails();
 SortByDate();
+zoeken();
