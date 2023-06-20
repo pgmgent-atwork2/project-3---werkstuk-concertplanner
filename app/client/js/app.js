fb325e2e99b39a180e5f12f896bd237aa615cf95
Lennert Vermoesen <113594885+Softie123456789@users.noreply.github.com>
2023-06-20 10:59:24 +0200

    fix

--- app/client/js/app.js
+++ app/client/js/app.js
@@ -1,10 +1,9 @@
 import { renderSlideShow } from "./animations/slideshow.js";
 import { smoothScroll } from "./animations/smooth-scroll.js";
 import { dragging } from "./dragging.js";
-import { SVG } from "../../pixi-svg";
 
 renderSlideShow();
 smoothScroll();
 dragging();
-SVG
+
 
